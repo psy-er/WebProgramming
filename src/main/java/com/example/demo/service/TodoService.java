b810f39e90ea7752b79610f5f713e18ef206d691
psy-er <89902211+psy-er@users.noreply.github.com>
2024-03-25 10:00:27 +0900

    init

---
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+// 서비스에서 비즈니스 로직 구현
+@Service
+public class TodoService {
+    public String testService(){
+        return "Test service";
+    }
+}
