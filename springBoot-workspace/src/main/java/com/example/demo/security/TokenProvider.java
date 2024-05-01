34e55918d09a874df01e372582dd37d58664e15d
psy-er <89902211+psy-er@users.noreply.github.com>
2024-05-01 12:31:57 +0900

    security 설정

---
+++ springBoot-workspace/src/main/java/com/example/demo/security/TokenProvider.java
@@ -0,0 +1,44 @@
+package com.example.demo.security;
+
+
+import com.example.demo.model.UserEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Slf4j
+@Service
+public class TokenProvider {
+    private static final String SECRET_KEY = "dkfspdoivlkrsn;lksmoig43yg9hdkgn";
+
+    public String create(UserEntity userEntity){
+        // 기한 지금으로 부터 1일로 설정
+        Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
+
+        // JWT Token 생성
+        return Jwts.builder()
+                // header에 들어갈 내용 및 서명을 하기 위한 SECRET_KEY
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                // payload에 들어갈 내용
+                .setSubject(userEntity.getId()) // sub
+                .setIssuer("demo app") // iss
+                .setIssuedAt(new Date()) //iat
+                .setExpiration(expiryDate) // exp
+                .compact();
+    }
+    public String validateAndGetUserId(String token){
+        // parseClaimsJws 메서드가 Base 64로 디코딩 및 파싱.
+        // 즉, 헤더와 페이로드를 setSigningKey로 넘어온 시크릿을 이용해 서명 후, token의 서명과 비교
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+}
